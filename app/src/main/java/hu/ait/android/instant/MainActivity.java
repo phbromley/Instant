de8772ea447ef2fffd77723c3958ca983149f29e
Matt Kohn <matt.cal.kohn@gmail.com>
2017-12-07 14:17:11 +0100

    random notes

--- app/src/main/java/hu/ait/android/instant/MainActivity.java
+++ app/src/main/java/hu/ait/android/instant/MainActivity.java
@@ -6,6 +6,19 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+    * this is going to be a splash screen where it checks using SharedPreferences
+    *   if the person is logged in or not
+    *
+    *   if logged in, transitions after 3 seconds to feed
+    *
+    *   if not logged in, transitions after 3 seconds to login page
+    *
+    *   @Peter is going to do this and then setup the login activity
+    *   - also throw in an animation on splash screen that has the Instant logo pls
+    *
+    * */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
