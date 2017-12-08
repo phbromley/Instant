35eac6823e63d591fc8a206e210758346d434eb5
mchen16 <michael_chen1@brown.edu>
2017-12-08 18:59:24 +0100

    fragments

---
+++ app/src/main/java/hu/ait/android/instant/FragmentFeed.java
@@ -0,0 +1,109 @@
+package hu.ait.android.instant;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import hu.ait.android.instant.adapter.PostsAdapter;
+import hu.ait.android.instant.data.Post;
+
+/**
+ * Created by mchen16 on 12/7/17.
+ */
+
+public class FragmentFeed extends Fragment {
+
+    public static final String TAG = "FragmentFeed";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View viewRoot = inflater.inflate(R.layout.fragment_feed, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) getActivity().findViewById(R.id.recyclerViewPosts2);
+        adapter = new PostsAdapter(getActivity(), FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+        initPostsListener();
+
+        return viewRoot;
+    }
+
+    PostsAdapter adapter;
+
+
+    private void initPostsListener() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("posts");
+        reference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Post post = dataSnapshot.getValue(Post.class);
+                adapter.addPost(post, dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                adapter.removePostByKey(dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+//    @SuppressWarnings("StatementWithEmptyBody")
+//    @Override
+//    public boolean onNavigationItemSelected(MenuItem item) {
+//        // Handle navigation view item clicks here.
+//        int id = item.getItemId();
+//
+//        if (id == R.id.nav_logout) {
+//            FirebaseAuth.getInstance().signOut();
+//            finish();
+//        }
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        drawer.closeDrawer(GravityCompat.START);
+//        return true;
+//    }
+}
