5fc76942357b161284af791ff8561f629c86fa3b
Matt Kohn <matt.cal.kohn@gmail.com>
2017-12-12 18:59:05 +0100

    pretty sure feed shows only who you follow - someone add more accounts pls

--- app/src/main/java/hu/ait/android/instant/FragmentFeed.java
+++ app/src/main/java/hu/ait/android/instant/FragmentFeed.java
@@ -27,13 +27,22 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import hu.ait.android.instant.adapter.PostsAdapter;
+import hu.ait.android.instant.data.DataManager;
 import hu.ait.android.instant.data.Post;
+import hu.ait.android.instant.data.User;
 
 
 public class FragmentFeed extends Fragment {
 
     public static final String TAG = "FragmentFeed";
+
+    private PostsAdapter adapter;
+    private Set<String> feedIDs;
+    private User currentUser;
 
     @Nullable
     @Override
@@ -41,22 +50,35 @@ public class FragmentFeed extends Fragment {
 
         View viewRoot = inflater.inflate(R.layout.fragment_feed, container, false);
 
-        RecyclerView recyclerView = (RecyclerView) viewRoot.findViewById(R.id.recyclerViewPosts2);
-        adapter = new PostsAdapter(getActivity(), FirebaseAuth.getInstance().getCurrentUser().getUid());
+        currentUser = DataManager.getInstance().getCurrentUser();
+        feedIDs = new HashSet<>();
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        layoutManager.setReverseLayout(true);
-        layoutManager.setStackFromEnd(true);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
+        initRecyclerView(viewRoot);
 
         initPostsListener();
 
         return viewRoot;
     }
 
-    private PostsAdapter adapter;
+    private void reinitFeedIDs() {
 
+        feedIDs.add(currentUser.getUId());
+
+        for(User user: currentUser.getFollowing()) {
+            feedIDs.add(user.getUId());
+        }
+    }
+
+    private void initRecyclerView(View view) {
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewPosts2);
+        adapter = new PostsAdapter(getActivity(), currentUser.getUId());
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
 
     private void initPostsListener() {
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("posts");
@@ -64,7 +86,11 @@ public class FragmentFeed extends Fragment {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Post post = dataSnapshot.getValue(Post.class);
-                adapter.addPost(post, dataSnapshot.getKey());
+
+                reinitFeedIDs();
+
+                if(feedIDs.contains(post.getUid()))
+                    adapter.addPost(post, dataSnapshot.getKey());
             }
 
             @Override
@@ -74,6 +100,7 @@ public class FragmentFeed extends Fragment {
 
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
+                // might be issue if someone deletes something on an account a person does not follow
                 adapter.removePostByKey(dataSnapshot.getKey());
             }
 
